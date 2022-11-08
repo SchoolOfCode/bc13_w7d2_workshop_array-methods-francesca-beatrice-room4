dfaac2363a68b017e069aa11155d511e3eaa9891
Francesca Croutear-Foy <crofoys@gmail.com>
2022-11-08 12:15:00 +0000

    task 1 and 2

--- task1.js
+++ task1.js
@@ -1,3 +1,17 @@
 const cats = ["tony", "daisy", "socks", "rockie" ];
+const capitalisedCats = cats.map ( (cat) => {
+    console.log(cat.toUpperCase());
+})
 
-const capitalisedCats = // your code here
+const animals = [
+    "baboon",
+    "kangaroo",
+    "rhino",
+    "frog",
+    "beaver",
+    "horse",
+    "basilisk",
+  ];
+  const animalsB = animals.filter( (animal) => {
+    return animal.charAt(0)=='b'
+  })
